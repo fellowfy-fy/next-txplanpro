4e4861c01ddc7dca23dd8f341e7c7e418738e7ed
terrxrmachine <so3.workspace@gmail.com>
2024-09-30 21:38:29 +0400

    майнор чейнджес

--- app/all-plans/page.tsx
+++ app/all-plans/page.tsx
@@ -12,7 +12,6 @@ export default function AllPlans() {
   return (
     <Container>
       <Title text="All TxPlans" size="lg" className="font-bold" />
-      <PageDescription text="Unlock the world of web development effortlessly with our innovative e-learning courses. Elevate your skills, build a dynamic portfolio, and launch your web development or no-code career with our industry-aligned certifications and dedicated job placement assistance." size="xs" className="pb-3 lg:pb-0 max-w-[1200px]"/>
 
       <ElementGrid>
         <PatientCard />
