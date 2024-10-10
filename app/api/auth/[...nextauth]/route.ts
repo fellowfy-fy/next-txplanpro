51ea2e571be014a1f1c01c47f92259e593d1e251
fellowfy-fy <zhoraawesome@gmail.com>
2024-10-10 12:46:15 +0300

    Сгруппировал папки

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+// Конфигурация NextAuth
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+//sdfsdf
+//dsfsdfsdf
+///asdfsdfasdfsdf
+export { handler as GET, handler as POST };
