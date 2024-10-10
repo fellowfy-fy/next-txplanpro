7b48e2231ccad4d4678c0b99acc646aabe81f9b8
fellowfy-fy <zhoraawesome@gmail.com>
2024-10-10 16:29:27 +0300

    Добавил модели базы данных

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,59 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-// Конфигурация NextAuth
+import { prisma } from "@/prisma/prisma-client";
+import { compare, hashSync } from 'bcrypt';
+
 const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const values = {
+          email: credentials.email,
+        };
+
+        const findUser = await prisma.user.findFirst({
+          where: values,
+        });
+
+        if (!findUser) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(credentials.password, findUser.password);
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        if (!findUser.verified) {
+          return null;
+        }
+
+        return {
+          id: findUser.id,
+          email: findUser.email,
+          name: findUser.fullName,
+          role: findUser.role,
+        };
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
 });
 
-//sdfsdf
-//dsfsdfsdf
-///asdfsdfasdfsdf
 export { handler as GET, handler as POST };
