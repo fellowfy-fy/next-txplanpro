74dd7097bc16ed02037ec55f2f2dcf97a8f4d423
fellowfy-fy <zhoraawesome@gmail.com>
2024-11-10 16:45:04 +0700

    дерьмо

--- src/app/dashboard/create-plan/page.tsx
+++ src/app/dashboard/create-plan/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Container } from "@/components/ui/container";
 import { CreatePlanForm } from "@/components/shared/create-plan-form";
 import { PageDescription } from "@/components/ui/page-description";
